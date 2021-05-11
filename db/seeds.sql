e7e229804351935805a5c73c191b1c4a5333854c
Andrew Burke <Drewburke39@gmail.com>
2021-05-11 12:04:51 -0600

    Still working on the functionality but I'm close

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Big Mac');
